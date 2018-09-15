86956309fe65cb78a8b38928489d6e79c161a2d4
bigO88 <rajjanwa@gmail.com>
2018-09-15 18:02:18 +0530

    Eureka service added

--- db-service/src/main/java/com/rmj/spring/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/rmj/spring/dbservice/DbServiceApplication.java
@@ -2,9 +2,11 @@ package com.rmj.spring.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
+@EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.rmj.spring.dbservice.repository")
 @SpringBootApplication
 public class DbServiceApplication {
--- stock-service/src/main/java/com/rmj/spring/stockservice/StockServiceApplication.java
+++ stock-service/src/main/java/com/rmj/spring/stockservice/StockServiceApplication.java
@@ -2,7 +2,10 @@ package com.rmj.spring.stockservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class StockServiceApplication {
 
